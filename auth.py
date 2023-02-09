bbf1b82421d8b5850dd5f1946f12303b9f5be9ca
Himanshu Vishwakarma <himanshu@Himanshus-MacBook-Pro.local>
2023-02-09 12:13:29 +0530

    Add the files

---
+++ auth.py
@@ -0,0 +1,56 @@
+import logging
+from kiteconnect import KiteConnect
+import json
+
+AUTH_DATA = json.load(open("secret.json"))
+print("API_KEY:", AUTH_DATA["API_KEY"])
+
+logging.basicConfig(level=logging.DEBUG)
+
+kite = KiteConnect(api_key=AUTH_DATA["API_KEY"])
+print("Login URL:", kite.login_url())
+
+# Redirect the user to the login url obtained
+# from kite.login_url(), and receive the request_token
+# from the registered redirect url after the login flow.
+# Once you have the request_token, obtain the access_token
+# as follows.
+
+data = kite.generate_session("request_token_here", api_secret=AUTH_DATA["API_SECRET"])
+print(data["access_token"])
+kite.set_access_token(data["access_token"])
+
+# Place an order
+try:
+    order_id = kite.place_order(tradingsymbol="YESBANK",
+                                exchange=kite.EXCHANGE_NSE,
+                                transaction_type=kite.TRANSACTION_TYPE_BUY,
+                                quantity=1,
+                                variety=kite.VARIETY_AMO,
+                                order_type=kite.ORDER_TYPE_MARKET,
+                                product=kite.PRODUCT_CNC,
+                                validity=kite.VALIDITY_DAY)
+
+    logging.info("Order placed. ID is: {}".format(order_id))
+except Exception as e:
+    logging.info("Order placement failed: {}".format(e.message))
+
+# Fetch all orders
+kite.orders()
+
+# Get instruments
+kite.instruments()
+
+# Place an mutual fund order
+kite.place_mf_order(
+    tradingsymbol="INF090I01239",
+    transaction_type=kite.TRANSACTION_TYPE_BUY,
+    amount=5000,
+    tag="mytag"
+)
+
+# Cancel a mutual fund order
+kite.cancel_mf_order(order_id="order_id")
+
+# Get mutual fund instruments
+kite.mf_instruments()
